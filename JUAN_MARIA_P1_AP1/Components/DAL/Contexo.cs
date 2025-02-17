6872bc84e000e3b01b8131c75250cae1429dcaa1
juanFelixM <juan_maria1@ucne.edu.do>
2025-02-17 19:07:04 -0400

    ConnectionStrings arreglado

---
+++ JUAN_MARIA_P1_AP1/Components/DAL/Contexo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace JUAN_MARIA_P1_AP1.Components.DAL
+{
+    public class Contexo
+    {
+        public class Contexto : DbContext
+        {
+            public Contexto(DbContextOptions<Contexto> Options) : base(Options) { }
+
+            public DbSet<Aportes> Tecnicos { get; set; }
+
+        }
+
+    }
+}
